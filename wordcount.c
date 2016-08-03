3459531f984d8473fff8a1026abe3ab223b5efa2
Eli Lichtblau <elilichtblau@gmail.com>
2016-08-02 20:17:32 -0400

    yea you got me

---
+++ wordcount.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+int main()
+{
+	char input[200];
+	
+	int Wcounter = 1;
+
+	FILE *inFile = fopen("text.txt", "r");
+	
+
+	for(int i = 0;i<sizeof(input);i++)
+	{
+		fscanf(inFile, "%c", &input[i]);
+	
+	}
+
+	for(int i = 0;i<sizeof(input);i++)
+	{
+		if(input[i] == ' ')
+		{
+			//printf("[%d]", input[i]);
+			for(int x = 2;x<sizeof(input);x++)
+			{
+				if(input[i+x] == ' ')
+				{
+					Wcounter+=1;
+					i = i+x-1;
+					break;
+				}
+								
+			}
+			
+		
+		}
+
+	}
+
+
+
+
+
+	printf("%d\n", Wcounter);
+
+
+
+
+
+	return 0;
+}
